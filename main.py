275b95cea1fbab0a7d5b85efa3c7c261e97fd575
SnowTempest <cdbbprogramming@gmail.com>
2024-10-04 21:30:02 -0500

    Initial Commit. Added logging system.

---
+++ main.py
@@ -0,0 +1,18 @@
+__author__ = "SnowTempest"
+__copyright__ = "Copyright (C) 2024 SnowTempest"
+__license__ = "GNU GENERAL PUBLIC LICENSE"
+__version__= "1.04"
+
+from bioshock_2_multiplayer_rpc import rpc_loop
+
+def main():
+    print("********************************************************************************************")
+    print(f'\nBioshock 2 Multiplayer Discord RPC v{__version__} by {__author__} (ADTempest on YT/Twitch)\n')
+    print("********************************************************************************************")
+    rpc_loop()
+
+if __name__ == "__main__":
+    main()
+
+
+
